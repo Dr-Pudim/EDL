2430830e1bc765fa91e360f62f036b846e46b559
Renan <renan.coding@gmail.com>
2018-01-29 10:38:10 -0200

    [CORRIGIR] Tarefa 3 terminada

---
+++ tarefas/tarefa-03/main.lua
@@ -0,0 +1,6 @@
+function love.draw()
+    love.graphics.print("What is löve?", 200, 100)
+	love.graphics.print("Babe, don't hurt me", 300, 200)
+	love.graphics.print("Don't hurt me", 400, 300)
+	love.graphics.print("No more", 500, 400)
+end
